09cda85bb06a0505baf3a8132474ebaecda02a80
humtej1204 <humtej1204@gmail.com>
2021-09-30 23:39:28 +0000

    Creating the file and writing the code

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+*
+* Return: 0 number
+*/
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	printf("Size of int: %zu bytes\n", sizeof(intType));
+	printf("Size of float: %zu bytes\n", sizeof(floatType));
+	printf("Size of double: %zu bytes\n", sizeof(doubleType));
+	printf("Size of char: %zu byte\n", sizeof(charType));
+
+	return (0);
+}
