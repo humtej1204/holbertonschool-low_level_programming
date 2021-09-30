2db3d946fdce68f3d0dd55b2fb3486ece97b63ca
humtej1204 <humtej1204@gmail.com>
2021-09-30 23:45:22 +0000

    Correcting the code

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	double doubleType;
 	char charType;
 
-	printf("Size of int: %zu bytes\n", sizeof(intType));
-	printf("Size of float: %zu bytes\n", sizeof(floatType));
-	printf("Size of double: %zu bytes\n", sizeof(doubleType));
-	printf("Size of char: %zu byte\n", sizeof(charType));
+	printf("Size of int: %lu bytes\n", sizeof(intType));
+	printf("Size of float: %lu bytes\n", sizeof(floatType));
+	printf("Size of double: %lu bytes\n", sizeof(doubleType));
+	printf("Size of char: %lu byte\n", sizeof(charType));
 
 	return (0);
 }
