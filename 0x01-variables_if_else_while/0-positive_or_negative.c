e744d0e9327413acda12eff24d07ed4eea64909b
humtej1204 <humtej1204@gmail.com>
2021-10-02 02:26:49 +0000

    Creating the file and writing the code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: return one of the 3 options
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("is positive\n");
+	}
+	else if (n == 0)
+	{
+		printf("is zero\n");
+	}
+	else
+	{
+		printf("is negative\n");
+	}
+	return (0);
+}
