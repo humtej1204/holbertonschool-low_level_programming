a9c030183cc41625b17e65f26cee72a00f2a30eb
humtej1204 <humtej1204@gmail.com>
2021-10-02 03:38:55 +0000

    Creating the file and writing the code

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	char low;
+	char UP;
+
+	for (low = 'a'; low <= 'z'; ++low)
+		printf("%c", low);
+
+	for (UP = 'A'; UP <= 'Z'; ++UP)
+		printf("%c ", UP);
+
+	printf("\n");
+
+	return (0);
+}
