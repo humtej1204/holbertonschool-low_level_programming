052786f5a8ca272e47ed9eca282b0bdc309e809f
humtej1204 <humtej1204@gmail.com>
2021-10-02 04:15:00 +0000

    Correcting the code 0.3

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; ++c)
-		putchar(c);
-
-	if (c != 'e' || c != 'q')
 	{
-		putchar(c);
+		if (c != 'e' || c != 'q')
+		{
+			putchar(c);
+		}
 	}
 
 	printf("\n");
