d81c23257ed4de890df475c4d340d6148df91961
humtej1204 <humtej1204@gmail.com>
2021-10-02 03:49:16 +0000

    Creating the file and writing the code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end the program
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'a'; ++c)
+		printf("%c", c);
+
+	if (c == e || c == q)
+		continue;
+
+	return (0);
+}
