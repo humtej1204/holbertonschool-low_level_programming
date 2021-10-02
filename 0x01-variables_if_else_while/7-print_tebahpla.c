b021024963efbc97bdf5a57fa0f5bda343b26f32
humtej1204 <humtej1204@gmail.com>
2021-10-02 04:45:41 +0000

    Creating the file and writing the script

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a', --n)
+		putchar(n);
+
+	putchar(10);
+	return (0);
+}
