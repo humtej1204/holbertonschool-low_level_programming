0e3f7a59c8682b08f7120867af19f0049c99baad
humtej1204 <humtej1204@gmail.com>
2021-10-02 04:25:35 +0000

    Creating the file and writing the code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end of the program
+ */
+int main(void)
+{
+	int n;
+
+	printf("0")
+	for (n = 1; n <= 9; ++n)
+		printf("-%d", n);
+
+	return (0);
+}
