02c45d162ede7ad68fdeecb1ef0a750261ce4d3c
humtej1204 <humtej1204@gmail.com>
2021-10-02 04:33:03 +0000

    Creating the file and writing the code

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end of the program
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; ++n)
+		putchar(n);
+
+	printf("\n")
+
+	return (0);
+}
