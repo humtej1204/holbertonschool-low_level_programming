4fefcf912b965f814eff710b8e7e70e390b47c19
humtej1204 <humtej1204@gmail.com>
2021-10-02 04:57:10 +0000

    Correcting the code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - function
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	int a = '0';
+	int b = 'a';
+
+	while (a <= '9')
+		putchar(a);
+		a++;
+
+	while (b <= 'f')
+		putchar(b);
+		b++;
+
+	putchar(10);
+
+	return (0);
+}
