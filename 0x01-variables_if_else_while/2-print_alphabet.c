06d44c75dba2221d5c356d1345656d6510a2ebcc
humtej1204 <humtej1204@gmail.com>
2021-10-02 03:28:15 +0000

    Creating the file and writing the code

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: End the program
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'A'; letter <= 'Z'; ++letter)
+	printf("%c\n", letter);
+	return (0);
+}
