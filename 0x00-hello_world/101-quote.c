cbc86e1b46eb1f01fb2dbe7fc92fc6ed062797e9
humtej1204 <humtej1204@gmail.com>
2021-10-03 10:15:47 +0000

    Creating the file and writing the code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end program
+ */
+void main(void)
+{
+	char n = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fprintf(stderr, "%s", n);
+
+	return (1);
+}
