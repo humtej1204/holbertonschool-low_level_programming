5681c2ed33c8738091eb69c96e5d286482f6410a
humtej1204 <humtej1204@gmail.com>
2021-10-03 10:18:14 +0000

    Correcting the code

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  *
  * Return: end program
  */
-void main(void)
+int main(void)
 {
-	char n = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	fprintf(stderr, "%s", n);
 
