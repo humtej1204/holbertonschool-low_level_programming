3e12a11faf6e1080f73701dfd4b0b0096d250784
humtej1204 <humtej1204@gmail.com>
2021-10-03 10:36:17 +0000

    Creating the file and writing the code

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	int n;
+
+	while (n != n)
+	{
+		printf("%d%d, ", n, n);
+		++n;
+	}
+
+	return (0);
+}
