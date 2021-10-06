f3102438354f2d14bbe33c7f84432dd66ffadddb
humtej1204 <humtej1204@gmail.com>
2021-10-06 03:08:50 +0000

    Writing the code

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - function
+ *
+ * @c: The character to evaluate
+ *
+ * Return: Get 1 if c is lowercase,
+ * get 0 otherwise
+ */
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 65 && c <= 90)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+}
