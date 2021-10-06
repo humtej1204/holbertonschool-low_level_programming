507e0a90b81c3e7246db180a9f45c73106379c87
humtej1204 <humtej1204@gmail.com>
2021-10-06 04:37:52 +0000

    Correcting the code

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,9 +9,9 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
-int print_last_digit(int n);
+void jack_bauer(void);
 void times_table(void);
-void times_table(void);
+int add(int a, int b);
 void print_to_98(int n);
 
 #endif
