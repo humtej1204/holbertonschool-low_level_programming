2ce7f4c51a7b0d5632862f7fdac81181fcdbe2da
humtej1204 <humtej1204@gmail.com>
2021-10-06 04:31:57 +0000

    Correcting the code

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,11 +6,11 @@ void jack_bauer(void)
 {
 	int hour, h1, h2, min, m1, m2;
 
-	for (hour = 0, hour <= 23, ++hour)
+	for (hour = 0; hour <= 23; ++hour)
 	{
 		h1 = hour / 10;
 		h2 = hour % 10;
-		for (min = 0, min <= 60, ++min)
+		for (min = 0; min <= 60; ++min)
 		{
 			m1 = min / 10;
 			m2 = min % 10;
