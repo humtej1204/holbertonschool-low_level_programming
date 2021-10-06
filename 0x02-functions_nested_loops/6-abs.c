834dadec22290c3b5194c2768020a25cf6a29c18
humtej1204 <humtej1204@gmail.com>
2021-10-06 03:42:50 +0000

    Writing the code

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _abs - function to get the absolute value
+ *
+ * @int: parameter of the funtion
+ *
+ * Return: the absolute value
+ */
+int _abs(int)
+{
+	int n;
+
+	if (n < 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-n);
+	}
+}
+
