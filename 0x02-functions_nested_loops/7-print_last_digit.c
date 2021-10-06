e70044d966aac4c450cd4b4ea63d53cf1590e098
humtej1204 <humtej1204@gmail.com>
2021-10-06 03:51:55 +0000

    Writing the code 0.2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,11 @@
 #include "main.h"
-/**/
+/**
+ * print_last_digit - print the last digit
+ *
+ * @n: parameter
+ *
+ * Return: return the last digit
+ */
 int print_last_digit(int n)
 {
 	int d = n % 10;
