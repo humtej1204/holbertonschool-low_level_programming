1cadbab16fb37c6d1e1e94d16c39551c059a103d
humtej1204 <humtej1204@gmail.com>
2021-10-06 04:30:21 +0000

    Writing the code

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * jack_bauer - function
+ */
+void jack_bauer(void)
+{
+	int hour, h1, h2, min, m1, m2;
+
+	for (hour = 0, hour <= 23, ++hour)
+	{
+		h1 = hour / 10;
+		h2 = hour % 10;
+		for (min = 0, min <= 60, ++min)
+		{
+			m1 = min / 10;
+			m2 = min % 10;
+			_putchar(h1 + '0');
+			_putchar(h2 + '0');
+			_putchar(':');
+			_putchar(m1 + '0');
+			_putchar(m2 + '0');
+			_putchar(10);
+		}
+		min = 0;
+	}
+}
