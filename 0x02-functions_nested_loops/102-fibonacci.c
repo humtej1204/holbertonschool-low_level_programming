b4e57576b30c6adfd32b1f0025095c37d14646ee
humtej1204 <humtej1204@gmail.com>
2021-10-07 04:54:11 +0000

    Writing the code

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	long int n, a = 1, b = 1, s;
+	int c = 1;
+
+	printf("%ld", a);
+	for (n = 1; n < 50; ++n)
+	{
+		s = a + b;
+		printf(", %ld", s);
+		a = b;
+		b = s;
+	}
+	putchar(10);
+
+	return (0);
+}
