5c8ea7305c1484f2d015cc1aa9d0365b702630c4
humtej1204 <humtej1204@gmail.com>
2021-10-07 04:04:43 +0000

    Writing the code

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: end program
+ */
+int main(void)
+{
+	int n, s = 0, c = 1;
+
+	for (n = 0; n < 1024; ++n)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		{
+			s = s + n;
+		}
+		else
+		{
+
+		}
+	}
+	printf("%d\n", s);
+
+	return (0);
+}
