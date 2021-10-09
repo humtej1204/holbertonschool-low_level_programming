136c6ff7184f64147cede86ffcb979517d58fa11
humtej1204 <humtej1204@gmail.com>
2021-10-09 04:55:29 +0000

    Creating the file and writing the code

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: integer n to print using _putchar
+ *
+ * Return: none - void function
+ */
+void print_number(int n)
+{
+	int r;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar(45);
+	}
+	if (n == 0)
+		return;
+	r = n % 10;
+
+	print_number(n / 10);
+	_putchar(r + '0');
+}
