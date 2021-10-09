13ac1ceb8add43c80bf4a2672a9be0c3033e139e
humtej1204 <humtej1204@gmail.com>
2021-10-09 07:39:20 +0000

    Correcting the code 0.2

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,27 +7,22 @@
 void print_number(int n)
 {
 	int d, base10 = 1;
-	/* Si n inicial es negativo */
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	/* igualamos el valor de d con el valor inicial de n*/
 	d = n;
-	/* Obtenemos la notacion exponencial del numero */
 	while (d > 9)
 	{
 		d = d / 10;
 		base10 = base10 * 10;
 	}
-	/* Descomponemos el numero */
 	while (base10 > 1)
 	{
 		_putchar((n / base10) + '0');
 		n = n % base10;
 		base10 = base10 / 10;
 	}
-	/* imprimimos el ultimo digito del numero */
 	_putchar((n % 10) + '0');
 }
