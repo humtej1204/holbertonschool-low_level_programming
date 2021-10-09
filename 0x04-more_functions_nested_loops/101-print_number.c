5352bd1bc3555ec6f8abcf47f6c3783a6e3676d3
humtej1204 <humtej1204@gmail.com>
2021-10-09 07:49:00 +0000

    Correcting the code 0.4

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,35 @@
 #include "main.h"
 /**
- * print_number - function
- *
- * @n: parameter
+ * print_number - Print integer
+ * @n: number
  */
 void print_number(int n)
 {
-	int d, base10 = 1;
+	unsigned int n2, number, base10 = 1;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n2 = -n;
 	}
-	d = n;
-	while (d > 9)
+	else
 	{
-		d = d / 10;
+		n2 = n;
+	}
+	number = n2;
+	/* Obtenemos su base */
+	while (number > 9)
+	{
+		number = number / 10;
 		base10 = base10 * 10;
 	}
+	/* Descomponemos en base 10*/
+	number = n2;
 	while (base10 > 1)
 	{
-		_putchar((n / base10) + '0');
-		n = n % base10;
+		_putchar((number / base10) + '0');
+		number = number % base10;
 		base10 = base10 / 10;
 	}
-	_putchar((n % 10) + '0');
+	_putchar((n2 % 10) + '0');
 }
