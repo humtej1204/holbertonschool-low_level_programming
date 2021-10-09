3c3389abc891ba651942afd5b50ccd2c33440814
humtej1204 <humtej1204@gmail.com>
2021-10-09 03:44:56 +0000

    Creating the file and writing the code 0.2

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - main funcion
+ *
+ * Return: end program
+ * */
+int main()
+{
+	long int c, b, a = 612852475143;
+
+	while (a != 1)
+	{
+		for (b = 2; b <= a; ++b)
+		{
+			if ((a % b) == 0)
+			{
+				break;
+			}
+		}
+		c = (a / b);
+		a = c;
+	}
+	printf("%li\n", b);
+
+	return (0);
+}
