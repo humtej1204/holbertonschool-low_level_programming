9952fadabfcd86f9f90710bc66e953f5487ba16e
humtej1204 <humtej1204@gmail.com>
2021-10-19 04:09:23 +0000

    Correcting the code

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -3,7 +3,6 @@
 /**
  * print_diagsums - prints the sum of the two diagonals
  * of a square matrix of integers
- *
  * @a: square matrix of which we print the sum of diagonals
  * @size: size of the matrix
  */
