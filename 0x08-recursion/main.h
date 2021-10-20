5a6468a7783e77b5d9b0613949fffabd092cb678
humtej1204 <90359858+humtej1204@users.noreply.github.com>
2021-10-20 10:51:51 -0500

    Adding functions

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,13 +3,12 @@
 
 int _putchar(char c);
 
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 #endif
