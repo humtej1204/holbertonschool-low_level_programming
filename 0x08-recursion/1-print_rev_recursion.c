abc8589062d949c3683baafc257e3498850bfe4f
humtej1204 <humtej1204@gmail.com>
2021-10-20 18:07:47 +0000

    Writing the code

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - function that reverse
+ * the string
+ *
+ * @s: parameter
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != 0)
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}
+
+}
