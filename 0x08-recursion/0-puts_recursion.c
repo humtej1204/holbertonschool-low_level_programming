d9c417782a4503622cd19adefe0ab4c7893aeac5
humtej1204 <humtej1204@gmail.com>
2021-10-20 16:52:49 +0000

    Writing the code

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts_recursion - Function similar to puts
+ *
+ * @s: parameter
+ */
+void _puts_recursion(char *s)
+{
+	putchar(s[0]);
+	s++;
+	if (s[0] != '\0')
+	{
+		_puts_recursion(s);
+	}
+}
