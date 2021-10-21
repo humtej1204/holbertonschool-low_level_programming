0652b0c6713acfaeedebd014d6a1266168300be4
humtej1204 <90359858+humtej1204@users.noreply.github.com>
2021-10-20 23:03:35 -0500

    Create 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _sqrt - descripcion
+ * @x: number
+ * @y: number
+ * Return: suma de valores
+ */
+int _sqrt(int x, int y)
+{
+	int sum;
+  
+  sum = x - y;
+  if (sum < 0)
+		return (-(y / 2) - 1);
+	if (sum == 0)
+		return (1);
+	else
+		return (1 + _sqrt(sum, y + 2));
+}
+/**
+ * _sqrt_recursion - raiz cuadrada natural
+ * @n: number radicando
+ * Return: resultado
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	if (n == 1)
+		return (1);
+	return (_sqrt(n, 1));
+}
