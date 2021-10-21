c96dd2b42ce94de0130ef52a330d4d63d8a500ed
humtej1204 <90359858+humtej1204@users.noreply.github.com>
2021-10-20 23:21:08 -0500

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,12 +8,12 @@
  */
 int is_prime(int n, int div)
 {
-        if (div > (n / 2))
-                return (1);
-        if (n % div == 0)
-                return (0);
-        else
-                return (is_prime(n, div + 1));
+    if (div > (n / 2))
+        return (1);
+    if (n % div == 0)
+        return (0);
+    else
+        return (is_prime(n, div + 1));
 }
 /**
  * is_prime_number - identify the prime number
@@ -24,8 +24,8 @@ int is_prime(int n, int div)
  */
 int is_prime_number(int n)
 {
-        if (n == 0 || n == 1 || n < 0)
-                return (0);
-        else
-                return (is_prime(n, 2));
+    if (n == 0 || n == 1 || n < 0)
+        return (0);
+    else
+        return (is_prime(n, 2));
 }
