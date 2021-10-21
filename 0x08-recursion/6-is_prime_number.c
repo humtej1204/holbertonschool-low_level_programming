e092f65014c740da87d5f46ff8232625de80ba85
humtej1204 <90359858+humtej1204@users.noreply.github.com>
2021-10-20 23:23:03 -0500

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,27 @@
 #include "main.h"
 /**
- * is_prime - algorithm
- * @n: integer number
- * @div: divider
- *
- * Return: 1 if is prime. 0 other
+ * is_prime - primo
+ * @x: numero
+ * @y: numero
+ * Return: resultado 0 no primo 1 primo
  */
-int is_prime(int n, int div)
+int is_prime(int x, int y)
 {
-    if (div > (n / 2))
-        return (1);
-    if (n % div == 0)
-        return (0);
-    else
-        return (is_prime(n, div + 1));
+	if ((x % y) == 0 && y < x)
+		return (0);
+	if (x == y)
+		return (1);
+	else
+		return (is_prime(x, y + 1));
 }
 /**
- * is_prime_number - identify the prime number
- *
- * @n: integer number
- *
- * Return: 1 if is prime. 0 other
+ * is_prime_number - verifica si es numero primo
+ * @n: numero a verificar
+ * Return: resultado 0 no primo 1 primo
  */
 int is_prime_number(int n)
 {
-    if (n == 0 || n == 1 || n < 0)
-        return (0);
-    else
-        return (is_prime(n, 2));
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, 2));
 }
