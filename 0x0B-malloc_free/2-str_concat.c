e9936d5ed3ad9171fb69d8e71970ed71fae9dd45
humtej1204 <humtej1204@gmail.com>
2021-10-26 21:42:35 +0000

    Writing the code

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *str_concat - function  that returns a pointer
+ * to the content of 2 strings
+ * @s1: Parameter with a string
+ * @s2: Parameter with a string
+ * Return: Return the new array
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *a;
+	void *x = NULL;
+	int n1 = 0, n2 = 0, i = 0, i1 = 0, i2 = 0;
+
+	if (s1 != NULL)
+		while (s1[n1] != 0)
+			n1++;
+	else
+		s1 = "";
+	if (s2 != NULL)
+		while (s2[n2] != 0)
+			n2++;
+	else
+		s2 = "";
+
+	a = malloc(sizeof(char) * (n + 1));
+
+	if (a == NULL)
+	{
+		return (x);
+	}
+	else
+	{
+		while (i1 < n1)
+		{
+			a[i] = s1[i1];
+			i1++;
+			i++;
+		}
+		while (i2 <= n2)
+		{
+			a[i] = s2[i2];
+			i2++;
+			i++;
+		}
+		return (a);
+	}
+	return (0);
+}
