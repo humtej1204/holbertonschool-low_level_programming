f57bb528b066bf7da8839f6afdcd0320134820fb
humtej1204 <humtej1204@gmail.com>
2021-10-28 17:54:38 +0000

    Writing the code

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * *malloc_checked - Function that allocates memory
+ *
+ * @b: parameter
+ *
+ * Return: Return the pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int * a;
+	a = malloc (sizeof(unsigned int));
+
+	if (a == 0)
+	{
+		return (98);
+	}
+
+	return (a);
+}
