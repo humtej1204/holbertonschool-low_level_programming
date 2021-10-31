e6bd6df4048076d8136ff5c72f26bf12dbd7e021
humtej1204 <humtej1204@gmail.com>
2021-10-31 21:27:57 +0000

    Creating the Directory and files

---
+++ 0x0C-more_malloc_free/main.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *concat;
+
+	concat = string_nconcat("Best ", "School !!!", 6);
+	printf("%s\n", concat);
+	free(concat);
+
+	concat = string_nconcat("Holberton ", "School", 4);
+	printf("%s\n", concat);
+	free(concat);
+
+	concat = string_nconcat("", "Hello", 12);
+	printf("%s\n", concat);
+	free(concat);
+
+	concat = string_nconcat("Hello", "", 12);
+	printf("%s\n", concat);
+	free(concat);
+
+	concat = string_nconcat("Hello", NULL, 12);
+	printf("%s\n", concat);
+	free(concat);
+	concat = string_nconcat(NULL, "Hello", 0);
+	printf("%s\n", concat);
+	free(concat);
+	concat = string_nconcat(NULL, NULL, 10);
+	printf("%s\n", concat);
+	free(concat);
+
+	return (0);
+}
