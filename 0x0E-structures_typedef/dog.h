4f2ee42471c0d1123f327169247f48161afcabb2
humtej1204 <humtej1204@gmail.com>
2021-11-01 16:52:07 +0000

    Writing the code

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - Create the structure dog
+ *
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Create the structure dog
+ */
+struct dog
+{
+	char * name;
+	float age;
+	char * owner;
+}
+
+#endif
