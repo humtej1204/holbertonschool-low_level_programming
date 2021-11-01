67922be60a16be39d1f42b9db6364963701ac9e1
humtej1204 <humtej1204@gmail.com>
2021-11-01 21:26:35 +0000

    Correcting the code 0.2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,27 +1,76 @@
+#include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "dog.h"
 /**
- * *new_dog - function that creates a new dog
+ * new_dog - create new data structure for dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog owner
  *
- * @name: Parameter with the name of the dog
- * @age: Parameter with the age of the dog
- * @owner: Parameter with the owner's name of the dog
- *
- * Return: End program
+ * Return: Pointer to new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
+	dog_t *new_dog;
+	int len_name, len_owner;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	len_name = _strlen(name);
+	new_dog->name = malloc(sizeof(char) * len_name + 1);
+	if (new_dog->name == NULL)
 	{
+		free(new_dog);
 		return (NULL);
 	}
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
+	new_dog->name = _strcpy(new_dog->name, name);
+	len_owner = _strlen(owner);
+	new_dog->owner = malloc(sizeof(char) * len_owner + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 
-	return (dog);
+	new_dog->owner = _strcpy(new_dog->owner, owner);
+	new_dog->age = age;
+
+	return (new_dog);
+}
+
+/**
+ * _strlen - determinates the lenght of a string
+ * @s: pointer to string
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+	return (a);
+}
+
+/**
+ * _strcpy - copies a pointed string
+ * @dest: pointer to the destine string
+ * @src: pointer to the source string
+ * Return: the adress of the destiny string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
 }
