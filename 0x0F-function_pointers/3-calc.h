b150a6895cf1d24e08e04e32faa5ef3e88a97d55
humtej1204 <humtej1204@gmail.com>
2021-11-03 21:45:53 +0000

    Writing the code

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
+
+int (*get_op_func(char *s))(int, int);
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+#endif
