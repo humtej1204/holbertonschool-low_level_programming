623d57851744d900bb2a23965e6c50a7a28f586c
humtej1204 <humtej1204@gmail.com>
2021-11-04 19:46:29 +0000

    Creating the Head file

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
