e4717e6854ef2b39d6ec41e9fe788cf6d33f4736
humtej1204 <humtej1204@gmail.com>
2021-11-16 05:33:27 -0500

    Correcting the code

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,10 @@
  * myCleanupFun - function that prints somthing before
  * the main function is executed.
  */
-void myCleanupFun(void) __attribute__ ((destructor));
+void myCleanupFun(void) __attribute__ ((constructor));
 
 void myCleanupFun(void)
 {
-	printf("You're beat! and yet, you must allow\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
