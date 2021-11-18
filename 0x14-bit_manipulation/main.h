26e841c740f933ea28cf1409b2f00035a28dfeed
humtej1204 <humtej1204@gmail.com>
2021-11-18 17:07:21 -0500

    Creating the files and Directory

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <math.h>
+
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+#endif
