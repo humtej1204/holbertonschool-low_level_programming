31e09db36ae9e31669110769d8709feff2a63d95
humtej1204 <humtej1204@gmail.com>
2021-11-22 23:22:22 -0500

    Adding more code

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,25 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
+/* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+/* MACROS */
+#define BUFFERSIZE 1024
+#define READ_ERROR 1
+#define WRITE_ERROR 2
+
+/* prototypes of functions */
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *create_buffer(char *fileName);
+int error_handler(int fd, char *fname, int type, char *buffer);
+void close_file(int fd);
 
 #endif
