beefecb39f496eab14ab064e78507bac70dec02d
humtej1204 <humtej1204@gmail.com>
2021-11-22 20:55:33 -0500

    Creating the Directory and files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+#endif
