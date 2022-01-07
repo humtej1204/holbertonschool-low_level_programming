8f893100237c32d694be083b795a26448653897f
Humberto Tejada <humtej1204@gmail.com>
2022-01-06 23:17:14 -0500

    Correcting code

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,27 +1,39 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - adds a new node at the end
- * @head: dlistint_t list
- * @n: value
- * Return: address of the new node - NULL if it failed
+ * add_dnodeint_end - Adds a new node at the end of a dlistint_t list.
+ * @head: Double pointer to struct.
+ * @n: data to insert in the new node.
+ *
+ * Return: The address of the new element, or NULL if it failed.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-        dlistint_t *new, *tmp;
+	dlistint_t *node = NULL, *tmp = *head;
 
-        tmp = *head;
-        new = malloc(sizeof(dlistint_t));
-        if (new == NULL)
-                return (NULL);
-        while (tmp != NULL && tmp->next != NULL)
-                tmp = tmp->next;
-        new->n = n;
-        new->prev = tmp;
-        new->next = NULL;
-        if (tmp != NULL)
-                tmp->next = new;
-        else
-                *head = new;
-        return (new);
+	if (head == NULL)
+		return (NULL);
+
+	node = malloc(sizeof(dlistint_t));
+	if (!node)
+	{
+		dprintf(2, "Error: Can't malloc\n");
+		return (NULL);
+	}
+	node->n = n;
+	node->next = NULL;
+	if (*head == NULL)
+	{
+		node->prev = NULL;
+		(*head) = node;
+		return (node);
+	}
+
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = node;
+	node->prev = tmp;
+	return (node);
 }
