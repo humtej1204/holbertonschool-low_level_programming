2bf70b549ec6f5920f7ab6fbb8c4d9d6595b5f7d
Humberto Tejada <humtej1204@gmail.com>
2022-05-04 00:47:34 -0500

    Creating file and writting code

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * binary_search - function that searches for a value in an array
+ * of integers using the Linear search algorithm
+ *
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ *
+ * Return: return the first index where value is located
+ **/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array)
+		return (-1);
+	
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+
+	return (-1);
+}
