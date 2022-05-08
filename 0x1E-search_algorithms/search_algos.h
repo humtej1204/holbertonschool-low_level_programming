2c1d1f1428c97d64988975d0a616687bed26d36e
Humberto Tejada <humtej1204@gmail.com>
2022-05-07 23:24:30 -0500

    Adding new prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,6 +12,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 int b_search(int *array, int l, int r, size_t size, int value);
 void print_array(int *start, int *end);
